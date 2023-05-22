79c5d151092ac11cb3fa60cef0c45fe8561cd7ed
Victor-GameDeveloper <104779446+Victor-GameDeveloper@users.noreply.github.com>
2023-05-22 20:34:53 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+a = 33
+b = 33
+
+if b > a:
+    print("b is greater that a")
+elif b == a:
+    print("a and b are equal")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + "Refsnes")
+
+my_function("emil")
+my_function("tobias")
+my_function("linus")
